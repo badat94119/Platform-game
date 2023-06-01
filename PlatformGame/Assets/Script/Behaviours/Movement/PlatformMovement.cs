df59742bfb5fa5752400c6ea5de1e937593a0606
badat94119 <108143698+badat94119@users.noreply.github.com>
2023-06-01 21:17:08 +0700

    Add to github

---
+++ PlatformGame/Assets/Script/Behaviours/Movement/PlatformMovement.cs
@@ -0,0 +1,91 @@
+using Enums;
+using Extensions;
+using Interfaces;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(InputProvider))]
+public class PlatformMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private InputProvider _inputProvider;
+    private ICheck _groundCheck;
+    private Animator _animator;
+    private float _inputX;
+
+    [Header("Movement Configuration")]
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    [SerializeField]
+    private GameObject groundCheckObject;
+
+    private static readonly int IsRunning = Animator.StringToHash("IsRunning");
+    private static readonly int Grounded = Animator.StringToHash("IsGrounded");
+    private static readonly int VerticalVelocity = Animator.StringToHash("VerticalVelocity");
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _inputProvider = GetComponent<InputProvider>();
+        _groundCheck = groundCheckObject.GetComponent<ICheck>();
+        _animator = GetComponent<Animator>();
+}
+
+    private void FixedUpdate()
+    {
+        ApplyHorizontalMovement();
+        ApplyJump();
+    }
+
+    private void Update()
+    {
+        CaptureHorizontalInput();
+        ApplyWalkingDirection();
+        ApplyAnimations();
+    }
+
+    private void ApplyJump()
+    {
+        if (IsGrounded() && _inputProvider.GetActionPressed(InputAction.Jump))
+        {
+            _rigidbody.SetVelocity(Axis.Y, jumpForce);
+            _animator.SetTrigger("Jump");
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        return _groundCheck.Check();
+    }
+
+    private void ApplyHorizontalMovement()
+    {
+        _rigidbody.SetVelocity(Axis.X, _inputX * walkSpeed);
+    }
+
+    private void ApplyWalkingDirection()
+    {
+        if (_inputX != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(_inputX), 1, 1);
+        }
+
+    }
+
+    private void CaptureHorizontalInput()
+    {
+        _inputX = _inputProvider.GetAxis(Axis.X);
+    }
+
+    private void ApplyAnimations()
+    {
+        _animator.SetBool(IsRunning, _inputX != 0);
+        _animator.SetBool(Grounded, IsGrounded());
+        _animator.SetFloat(VerticalVelocity, _rigidbody.velocity.y);
+    }
+}
